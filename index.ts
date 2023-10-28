1f1bf690252b3b06c428a30f0739c1a0c461bdb1
robin marillia <robin4a4@gmail.com>
2023-10-29 01:07:31 +0200

    test typings

--- index.ts
+++ index.ts
@@ -3,3 +3,4 @@ export { Layout } from "./src/Layout";
 export { useQueryData, useMutationData } from "./src/hooks";
 export { JsonResponse } from "./src/responses";
 export { clientLiveReload } from "./src/dev-tools/clientLiveReload";
+export * from "./src/types";
