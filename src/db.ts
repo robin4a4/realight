22b68877c4e029a123ed46e511b01f2dde4bd8d2
robin marillia <robin4a4@gmail.com>
2023-10-30 22:56:22 +0100

    try something with imports

--- src/db.ts
+++ src/db.ts
@@ -1,9 +1,9 @@
-import type Database from "bun:sqlite";
+import Database from "bun:sqlite";
 
-let db = null;
+const db: { db: Database | null } = { db: null };
 if (typeof document === "undefined") {
 	const { Database } = await import("bun:sqlite");
-	db = new Database("./data/mydb.sqlite", { create: true });
+	db.db = new Database("./data/mydb.sqlite", { create: true });
 }
 
-export default db as Database;
+export default db;
