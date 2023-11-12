7c945c178bd351fe18fb0ba64962ba662390291a
robin marillia <robin4a4@gmail.com>
2023-11-12 16:36:28 +0100

    export RedirectResponse

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 export { useForm } from "./src/Form";
 export { Layout } from "./src/Layout";
 export { useQueryData, useMutationData } from "./src/hooks";
-export { JsonResponse } from "./src/responses";
+export { JsonResponse, RedirectResponse } from "./src/responses";
 export { clientLiveReload } from "./src/dev-tools/clientLiveReload";
 export * from "./src/types";
